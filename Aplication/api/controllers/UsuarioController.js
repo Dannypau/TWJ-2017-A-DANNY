093b943b60a2fa4a89d437df31ca49e95273b306
Dannypau <USRDEL@USR8.local>
2017-05-02 15:28:36 -0500

    actualización de api usuario

--- Aplication/api/controllers/UsuarioController.js
+++ Aplication/api/controllers/UsuarioController.js
@@ -1,5 +1,5 @@
 /**
- * UsuariosController
+ * UsuarioController
  *
  * @description :: Server-side logic for managing Usuarios
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
